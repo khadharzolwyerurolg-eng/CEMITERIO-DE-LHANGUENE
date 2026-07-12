0748b2bbcaea8d5aa6d0cb7dbc9874c4c3c62e63
khadharzolwyerurolg-eng <khadharzolwyerurolg@gmail.com>
2026-07-12 08:02:02 +0200

    Adicionando os arquivos de visualização restantes

--- spreadsheet/api.php
+++ spreadsheet/api.php
@@ -31,8 +31,7 @@ if(count($_POST) > 0)
         $query = "SELECT * FROM users ORDER BY id DESC";
         $result = query($query);
         $info['data'] = $result ?? [];
-    }else
-    if($info['data_type'] == 'delete')
+    }else if($info['data_type'] == 'delete')
     {
         $id = intval($_POST['id']);
 
@@ -47,6 +46,45 @@ if(count($_POST) > 0)
 
         $query = "DELETE FROM users WHERE id = $id LIMIT 1";
 
+        $result = query($query);
+        $info['data'] = $result ?? [];
+    }elseif($info['data_type'] == 'update')
+    {
+        $id = intval($_POST['id']);
+        $current = query("SELECT image FROM users WHERE id = $id LIMIT 1");
+        $image = '';
+        if (is_array($current) && count($current) > 0) {
+            $image = $current[0]['image'] ?? '';
+        }
+
+        if (!empty($_FILES['image']['name'])) {
+            $allowed = ['image/jpeg', 'image/png'];
+
+            if(in_array($_FILES['image']['type'], $allowed))
+            {
+                $folder = "uploads/";
+                if(!file_exists($folder))
+                {
+                    mkdir($folder, 0777, true);
+                }
+
+                $path = $folder . time() . $_FILES['image']['name'];
+                move_uploaded_file($_FILES['image']['tmp_name'], $path);
+
+                if ($image && file_exists($image)) {
+                    @unlink($image);
+                }
+
+                $image = $path;
+            }
+        }
+
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+
+        $query = "UPDATE users SET name='$name', age='$age', image='$image', email='$email', city='$city' WHERE id = $id LIMIT 1";
         $result = query($query);
         $info['data'] = $result ?? [];
     }elseif($info['data_type'] == 'save')
